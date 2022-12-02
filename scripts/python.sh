e7a8bed639cef8098ae42488b580aa1df8276056
SriMadhanReddy <115637314+SriMadhanReddy@users.noreply.github.com>
2022-12-02 19:38:33 +0000

    Update python.sh

--- scripts/python.sh
+++ scripts/python.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python /home/ec2-user/workspace/Devops_pipeline/manage.py runserver 0.0.0.0:8000 &
+python3 /home/ec2-user/workspace/Devops_pipeline/manage.py runserver 0.0.0.0:8000 &
