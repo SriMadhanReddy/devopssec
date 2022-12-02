1aa5e7ed73852aca4c93f857ee7bc77324d7053c
SriMadhanReddy <115637314+SriMadhanReddy@users.noreply.github.com>
2022-12-02 19:26:47 +0000

    Update python.sh

--- scripts/python.sh
+++ scripts/python.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python /home/ec2-user/workspace/Django-pipeline/django/manage.py runserver 0.0.0.0:8000 &
+python /home/ec2-user/workspace/Devops_pipeline/manage.py runserver 0.0.0.0:8000 &
